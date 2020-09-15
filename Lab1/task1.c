c59082892ca2c709d2cd36376afb7540368d66ca
Kevin Jaipersaud (csc103 student) <kjaiper000@citymail.cuny.edu>
2020-09-15 13:54:38 -0400

    First Commit

---
+++ Lab1/task1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+
+int main (int argc, char* argv[])
+{
+
+/* Part 0 */
+  char* filepath = argv[1];
+  int returnval;
+
+  // Check file existence
+  returnval = access (filepath, F_OK);
+  if (returnval == 0)
+    printf ("\n %s exists\n", filepath);
+  else
+  {
+    if (errno == ENOENT)
+      printf ("%s does not exist\n", filepath)
+    else if (errno == EACCES)
+    {
+      printf ("%s is not accessible\n", filepath);
+      return 0;
+    }
+  }
+
+  //Check read access...
+  //Check write access..
+}
